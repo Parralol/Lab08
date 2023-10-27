ad1397d31f14651676f3a5007c0a8e27c59b5484
Parralol <110953563+Parralol@users.noreply.github.com>
2023-10-26 22:14:43 -0500

    CONECTA

--- src/main/java/co/edu/escuelaing/cvds/lab7/Lab7Application.java
+++ src/main/java/co/edu/escuelaing/cvds/lab7/Lab7Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import co.edu.escuelaing.cvds.lab7.service.EmployeeService;
-
+//Lab081324
 @SpringBootApplication
 public class Lab7Application {
 	@Autowired
@@ -14,5 +14,4 @@ public class Lab7Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Lab7Application.class, args);
 	}
-
 }
